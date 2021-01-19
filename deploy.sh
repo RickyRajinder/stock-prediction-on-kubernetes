161a5148fac1ae8abe946f1f04d71d5516fcfec7
RickyRajinder <singh.sangh@gmail.com>
2021-01-18 22:42:33 -0800

    fix secrets

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t rickywraith/stock-predict-dm:latest -t rickywraith/stock-predict-dm:$SHA -f ./Data_Model_Dockerfile .
-docker build -t rickywraith/stock-predict-s3:latest -t rickywraith/stock-predict-s3:$SHA -f ./S3_Dockerfile .
+docker build --build-arg AWS_SECRET_KEY=$AWS_SECRET_KEY --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -t rickywraith/stock-predict-dm:latest -t rickywraith/stock-predict-dm:$SHA -f ./Data_Model_Dockerfile .
+docker build --build-arg AWS_SECRET_KEY=$AWS_SECRET_KEY --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -t rickywraith/stock-predict-s3:latest -t rickywraith/stock-predict-s3:$SHA -f ./S3_Dockerfile .
 docker build -t rickywraith/stock-predict-ws:latest -t rickywraith/stock-predict-ws:$SHA -f ./Webserver_Dockerfile .
 docker push rickywraith/stock-predict-dm:latest
 docker push rickywraith/stock-predict-s3:latest
